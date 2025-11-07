2ff76b41f7ac674757cd240631e73195738475da
Fabian-85 <fabian.maier0805@hotmail.com>
2025-11-07 11:54:01 +0100

    feat(model): add mdeol for a todo item

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+status_choices = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+]
+
+
+class Todo(models.Model):
+
+    """
+    Model representing a to-do item.
+
+    Attributes:
+        title (str): The title of the todo item.
+        description (str): A detailed description of the todo item.
+        status (str): The current status of the to-do item, chosen from predefined options (pending, in_progress, completed).
+    """
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    status = models.CharField(choices=status_choices, default='pending')
+
+    def __str__(self):
+        return self.title
