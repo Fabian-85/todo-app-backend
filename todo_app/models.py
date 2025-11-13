d384def01b468a654e76217422e1525292e5e88c
Fabian-85 <fabian.maier0805@hotmail.com>
2025-11-13 11:47:39 +0100

    feat(todo-model): add review as choice for status in todo model

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 status_choices = [
     ('pending', 'Pending'),
     ('in_progress', 'In Progress'),
-    ('completed', 'Completed'),
+    ('review','Review'),
+    ('completed', 'Completed')
 ]
 
 
